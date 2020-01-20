5080ef4f086e7a3585d0ee243cfad76abdbd2e5e
PyroNage <marcodambrosio96@gmail.com>
2020-01-19 22:46:24 -0500

    Commented code properly

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler #Built into the python HTTP library, handles the HTTP requests
 
 
-class Serv(BaseHTTPRequestHandler):
+class Serv(BaseHTTPRequestHandler):                     #Declare class that handles the serving of the content
 
     def do_GET(self):
-        if self.path == '/':
-            self.path = '/index.html'
+        if self.path == '/':                            #Check path, if it's /, index page
+            self.path = '/app.html'
         try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
+            file_to_open = open(self.path[1:]).read()   #Try to open app.html
+            self.send_response(200)                     #reply success code
         except:
-            file_to_open = "File not found"
-            self.send_response(404)
+            file_to_open = "File not found"             #Error opening file
+            self.send_response(404)                     #reply file not found code
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.wfile.write(bytes(file_to_open, 'utf-8'))  #Write contents of file to screen (utf-8 encoding)
 
 
 httpd = HTTPServer(('localhost', 8080), Serv)
