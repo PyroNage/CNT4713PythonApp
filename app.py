b78808e49d697cceeb49fcdf9bf19ed09d238264
PyroNage <marcodambrosio96@gmail.com>
2020-01-22 14:11:43 -0500

    Posting for calss

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<string:page_name>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)</string:page_name>
+@app.route("/")
+def hello():
+    return "Hello from Python, prof Ruben!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
