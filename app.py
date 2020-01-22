3ad3b89181be05682e1ccad3b7456d216a9a36f9
PyroNage <marcodambrosio96@gmail.com>
2020-01-22 12:35:09 -0500

    It did not work, commiting to previous version so i can try to create more pages on this 2.7 flask version

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import os
-from http.server import HTTPServer, BaseHTTPRequestHandler #Built into the python HTTP library, handles the HTTP requests
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello from Python, prof Ruben!"
 
-class Serv(BaseHTTPRequestHandler):                     #Declare class that handles the serving of the content
-
-    def do_GET(self):
-        if self.path == '/':                            #Check path, if it's /, index page
-            self.path = '/app.html'
-        try:
-            file_to_open = open(self.path[1:]).read()   #Try to open app.html
-            self.send_response(200)                     #reply success code
-        except:
-            file_to_open = "File not found"             #Error opening file
-            self.send_response(404)                     #reply file not found code
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))  #Write contents of file to screen (utf-8 encoding)
-
-
-httpd = HTTPServer((int(os.environ.get("PORT", 5000))), Serv)
-httpd.serve_forever()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
