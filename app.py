f0d620c99bec36332363015fac119fee78fba2e8
PyroNage <marcodambrosio96@gmail.com>
2020-01-22 12:46:23 -0500

    Added a new directory and changed the routing to work with dynamic html routing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Python, prof Ruben!"
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+    return render_template('%s.html' % page_name)</string:page_name>
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
